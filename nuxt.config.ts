f1be84d045fdbd35c1181029a5e18d5ae3107448
Aleksey Alekseev <8a7a11g@gmail.com>
2025-04-02 13:47:44 +0300

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui-pro',
     '@vueuse/nuxt',
     '@nuxtjs/device',
   ],
+  ui: {
+    colorMode: false
+  },
 
   vite: {
     css: {
